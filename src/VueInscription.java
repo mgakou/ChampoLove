b85fac7c1bf7965418957fd2fcc1ee5696abd7d5
mgakou <mhmdgakou@gmail.com>
2023-03-03 16:04:57 +0100

    L'interface ressemble a celui d'un smartphone + ajout des boutons homme et femme

--- src/VueInscription.java
+++ src/VueInscription.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,37 +11,53 @@ public class VueInscription extends JPanel{
 
 	public VueInscription() {
 		// TODO Auto-generated constructor stub
-		this.setLayout(new BorderLayout());;
 		JLabel titre = new JLabel("Parler nous de vous !");
-		titre.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		//this.add(titre,BorderLayout.NORTH);
+		titre.setFont(new Font("Tahoma", Font.PLAIN, 38));
+		this.add(titre);
 		
 		JLabel labelNom = new JLabel("Nom");
 		labelNom.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		labelNom.setBounds(200, 300, 50, 50);
 		this.add(labelNom);
 		
 		JTextField champsNom = new JTextField(25);
 		this.add(champsNom);
 		
-		JTextField surnameField = new JTextField(25);
-		JTextField countryField = new JTextField(25);
-		JTextField phoneNumberField = new JTextField(25);
-		JTextField ageField = new JTextField(25);
-
+		JLabel labelPrenom = new JLabel("Prénom");
+		labelPrenom.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.add(labelPrenom);
 		
+		JTextField champsPrenom = new JTextField(25);
+		this.add(champsPrenom);
 		
-//		
-//		
-//		this.add(labelPrenom);
-//		this.add(labelPays);
-//		this.add(labelNumero);
-//		this.add(labelAge);
-//		
-//		this.add(surnameField);
-//		this.add(countryField);
-//		this.add(phoneNumberField);
-//		this.add(ageField);
+		JLabel labelPays = new JLabel("Pays");
+		labelPays.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.add(labelPays);
+		
+		JTextField champsPays = new JTextField(25);
+		this.add(champsPays);
+		
+		JLabel labelNum = new JLabel("Numéro de Tél");
+		labelNum.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.add(labelNum);
+		
+		JTextField champsNum = new JTextField(25);
+		this.add(champsNum);
+		
+		JLabel labelAge = new JLabel("Age");
+		labelAge.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.add(labelAge);
+		
+		JTextField champsAge = new JTextField(25);
+		this.add(champsAge);
+		
+		JLabel labelGenre = new JLabel("Genre");
+		labelAge.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.add(labelGenre);
+		
+		JButton homme = new JButton("Homme");
+		JButton femme = new JButton("Femme");
+		//homme.setAlignmentY(BOTTOM_ALIGNMENT);
+		this.add(homme);
+		this.add(femme);
 	}
-
 }
