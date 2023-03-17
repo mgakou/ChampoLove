52011ec89eb5b5c306f2bc58e5c6b78e861623f7
mgakou <mhmdgakou@gmail.com>
2023-03-17 22:54:49 +0100

    Un peu plus agréable

--- src/VueInscription.java
+++ src/VueInscription.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -15,60 +17,73 @@ import javax.swing.SpinnerNumberModel;
 public class VueInscription extends JPanel{
 
 	public VueInscription() {
+	    // Panel principal
+	    this.setLayout(new GridLayout(8, 2, 10, 10));
+	    this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	    this.setBackground(new Color(240, 199, 199));
 
-        // Panel principal
-        //JPanel panel = new JPanel();
-        this.setLayout(new GridLayout(8, 2, 10, 10));
-        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	    // Label et champ Nom
+	    JLabel nomLabel = new JLabel("Nom : ");
+	    nomLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JTextField nomField = new JTextField(10);
+	    nomField.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(nomLabel);
+	    this.add(nomField);
 
-        // Label et champ Nom
-        JLabel nomLabel = new JLabel("Nom : ");
-        JTextField nomField = new JTextField(20);
-        this.add(nomLabel);
-        this.add(nomField);
+	    // Label et champ Prénom
+	    JLabel prenomLabel = new JLabel("Prénom : ");
+	    prenomLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JTextField prenomField = new JTextField(10);
+	    prenomField.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(prenomLabel);
+	    this.add(prenomField);
 
-        // Label et champ Prénom
-        JLabel prenomLabel = new JLabel("Prénom : ");
-        JTextField prenomField = new JTextField(20);
-        this.add(prenomLabel);
-        this.add(prenomField);
+	    // Label et champ Pays
+	    JLabel paysLabel = new JLabel("Pays : ");
+	    paysLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JTextField paysField = new JTextField(10);
+	    paysField.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(paysLabel);
+	    this.add(paysField);
 
-        // Label et champ Pays
-        JLabel paysLabel = new JLabel("Pays : ");
-        JTextField paysField = new JTextField(20);
-        this.add(paysLabel);
-        this.add(paysField);
+	    // Label et champ Ville
+	    JLabel villeLabel = new JLabel("Ville : ");
+	    villeLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JTextField villeField = new JTextField(10);
+	    villeField.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(villeLabel);
+	    this.add(villeField);
 
-        // Label et champ Ville
-        JLabel villeLabel = new JLabel("Ville : ");
-        JTextField villeField = new JTextField(20);
-        this.add(villeLabel);
-        this.add(villeField);
+	    // Label et champ Numéro de téléphone
+	    JLabel telLabel = new JLabel("Numéro de téléphone : ");
+	    telLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JTextField telField = new JTextField(10);
+	    telField.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(telLabel);
+	    this.add(telField);
 
-        // Label et champ Numéro de téléphone
-        JLabel telLabel = new JLabel("Numéro de téléphone : ");
-        JTextField telField = new JTextField(20);
-        this.add(telLabel);
-        this.add(telField);
+	    // Label et champ Genre
+	    JLabel genreLabel = new JLabel("Genre : ");
+	    genreLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JRadioButton hommeButton = new JRadioButton("Homme");
+	    JRadioButton femmeButton = new JRadioButton("Femme");
+	    ButtonGroup genreGroup = new ButtonGroup();
+	    genreGroup.add(hommeButton);
+	    genreGroup.add(femmeButton);
+	    
+	    JPanel genrePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    genrePanel.add(hommeButton);
+	    genrePanel.add(femmeButton);
+	    this.add(genreLabel);
+	    this.add(genrePanel);
 
-        // Label et champ Genre
-        JLabel genreLabel = new JLabel("Genre : ");
-        JRadioButton hommeButton = new JRadioButton("Homme");
-        JRadioButton femmeButton = new JRadioButton("Femme");
-        ButtonGroup genreGroup = new ButtonGroup();
-        genreGroup.add(hommeButton);
-        genreGroup.add(femmeButton);
-        JPanel genrePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        genrePanel.add(hommeButton);
-        genrePanel.add(femmeButton);
-        this.add(genreLabel);
-        this.add(genrePanel);
-
-        // Label et champ Age
-        JLabel ageLabel = new JLabel("Age : ");
-        JSpinner ageSpinner = new JSpinner(new SpinnerNumberModel(18, 18, 100, 1));
-        this.add(ageLabel);
-        this.add(ageSpinner);
+	    // Label et champ Age
+	    JLabel ageLabel = new JLabel("Age : ");
+	    ageLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    JSpinner ageSpinner = new JSpinner(new SpinnerNumberModel(18, 18, 100, 1));
+	    ageSpinner.setFont(new Font("Arial", Font.PLAIN, 14));
+	    this.add(ageLabel);
+	    this.add(ageSpinner);
 
         // Bouton Enregistrer
         JButton enregistrerButton = new JButton("Enregistrer");
