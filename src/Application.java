5890490bfabc094179e5ba616911764cba9eb6ef
HussamAghwan <79196384+HussamAghwan@users.noreply.github.com>
2023-03-22 11:14:50 +0100

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -14,6 +16,14 @@ public class Application extends JFrame{
 		this.setVisible(true);
 		this.setTitle("ChampoLove");
 		this.setSize(700,700);
+		this.setResizable(false);
+		
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = (int) screenSize.getWidth() / 2;
+        int centerY = (int) screenSize.getHeight() / 2;
+        this.setLocation(centerX - this.getWidth() / 2, centerY - this.getHeight() / 2);
+        
+        
 		this.setBackground(new Color(237,245,255));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
