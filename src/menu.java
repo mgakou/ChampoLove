54c3d540e5baef0832b8dd0605ac234965a28fc3
HussamAghwan <79196384+HussamAghwan@users.noreply.github.com>
2023-03-22 11:17:44 +0100

    Update menu.java

--- src/menu.java
+++ src/menu.java
@@ -1,40 +1,51 @@
-import java.awt.*;
 import javax.swing.*;
+
+import java.awt.*;
 import java.awt.event.*;
-		
-		public class menu extends JFrame {
-	
-			static JMenuBar mb;
-			static JMenu x;
-			static JMenuItem m1, m2, m3;
-			static JFrame f;
 
-			public static void main(String[] args) {
-			
-			      JFrame frame = new JFrame("ChampoLove");
-			      JMenuBar menu = new JMenuBar();
-			      JMenuItem inscription = new JMenuItem("inscrption");
-			      JMenu file = new JMenu("Inscrption");
-			      JMenuItem newf = new JMenuItem("s'incrire");
-			      JMenu contact = new JMenu("Contact");
-			      JLabel label= new JLabel();
-			      
+public class menu implements ActionListener {
+    
+    JFrame frame;
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+    
+    public menu() {
+        frame = new JFrame("Menu");
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        menuItem = new JMenuItem("Application");
+        menuItem.addActionListener(this);
+        
+        menu.add(menuItem);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        
+        frame.setSize(700, 700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
 
-			      menu.setLayout(new GridLayout(1,0));
-			      menu.add(file);
-
-			      file.add(newf);
-			      file.add(inscription);
-
-			      menu.add(contact);
-
-			      frame.setJMenuBar(menu);
-			      frame.setSize(350, 500);
-			      frame.setBackground(Color.red);
-			      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			      frame.setVisible(true);
-			  
-			}
-
-	}
+        
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = (int) screenSize.getWidth() / 2;
+        int centerY = (int) screenSize.getHeight() / 2;
+        frame.setLocation(centerX - frame.getWidth() / 2, centerY - frame.getHeight() / 2);
+        
+        
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == menuItem) {
+            Application otherPage = new Application();
+            otherPage.setVisible(true);
+            frame.setVisible(false);
+        }
+    }
+    
+    public static void main(String[] args) {
+        menu Menu = new menu();
+    }
+}
 
