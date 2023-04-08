c1887270773c2b5e57935da9497125a26fe5a92c
HussamAghwan <79196384+HussamAghwan@users.noreply.github.com>
2023-04-08 19:52:16 +0200

    Update PremiereVue.java

--- src/PremiereVue.java
+++ src/PremiereVue.java
@@ -1,49 +1,40 @@
 
 
 import java.awt.*;
-
 import javax.swing.*;
 
-
 public class PremiereVue extends JPanel {
-
-	JButton connectButton;
+    JButton connectButton;
 
     public PremiereVue() {
-
-        this.setLayout(new BorderLayout());
-        
+        this.setLayout(null); 
         this.setBackground(new Color(240, 199, 199));
 
-        
-
         JLabel textLabel = new JLabel("Bienvenue sur notre App !");
-
         textLabel.setFont(new Font("Arial", Font.BOLD, 20));
-
         textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        textLabel.setBounds(80, 0, 300, 50); 
+        this.add(textLabel); 
 
-        this.add(textLabel, BorderLayout.NORTH);
-
+        ImageIcon icon = new ImageIcon("image.png");
+        Image image = icon.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
+        ImageIcon resizedIcon = new ImageIcon(image);
+        JLabel label = new JLabel(resizedIcon);
+        label.setBounds(50, 40, resizedIcon.getIconWidth(), resizedIcon.getIconHeight()); 
+        this.add(label); 
 
         connectButton = new JButton("Se connecter");
-
-        
+        connectButton.setBounds(170, 500, 100, 50); 
 
         connectButton.addActionListener(e -> {
-        	DeuxiemeVue vue2 = new DeuxiemeVue();
+            DeuxiemeVue vue2 = new DeuxiemeVue();
             Application.getInstance().getContentPane().remove(PremiereVue.this);
             Application.getInstance().getContentPane().add(vue2);
             Application.getInstance().revalidate();
             Application.getInstance().repaint();
         });
-        
-        this.add(connectButton, BorderLayout.SOUTH);
-        
 
+        this.add(connectButton); 
     }
-
-
-
-
 }
+
