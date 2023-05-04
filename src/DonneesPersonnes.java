17a83485e6b7132678a693076aaebcf8df632543
mgakou <mhmdgakou@gmail.com>
2023-05-04 15:09:33 +0200

    Construction de la bdd dans modele

--- src/DonneesPersonnes.java
+++ src/DonneesPersonnes.java
@@ -16,18 +16,7 @@ public class DonneesPersonnes {
     public Personne getPersonne(String nom) {
         return personnes.get(nom);
     }
-    File img_p1 = new File("image_projet/homme_projet/1.jpg");
-    String[] affinite_p1 = {"Musique","Arts","Yoga","Salarié"};
-    Personne p1 = new Personne("Albert Law",25,000,affinite_p1,img_p1);
-    
 
-    File img_p2 = new File("image_projet/homme_projet/2.jpg");
-    String[] affinite_p2 = {"Photographie","Arts","Cuisine","Litterature","Sport"};
-    Personne p2 = new Personne("Albert Law",42,000,affinite_p2,img_p2);
-
-    File img_p3 = new File("image_projet/homme_projet/3.jpg");
-    String[] affinite_p3 = {"Vegetarien","Arts","Cuisine","Musique","Sport","Voyage"};
-    Personne p3 = new Personne("Albert Law",42,000,affinite_p3,img_p3);
     
     // Autres méthodes pour accéder et modifier les données des personnes
 }
--- src/Modele.java
+++ src/Modele.java
@@ -1,8 +1,28 @@
+import java.io.File;
 
 public class Modele {
+    private DonneesPersonnes donneesPersonnes;
+    
+    public Modele() {
+        donneesPersonnes = new DonneesPersonnes();
+        
+        File img_p1 = new File("image_projet/homme_projet/1.jpg");
+        String[] affinite_p1 = {"Musique","Arts","Yoga","Salarié"};
+        Personne p1 = new Personne("Albert Law",25,000,affinite_p1,img_p1);
+        donneesPersonnes.ajouterPersonne("Albert Law", p1);
+        
+        File img_p2 = new File("image_projet/femme_projet/1.jpg");
+        String[] affinite_p2 = {"Voyages", "Danse", "Cuisine", "Sport"};
+        Personne p2 = new Personne("Sophie Dupont", 30, 123456, affinite_p2, img_p2);
+        donneesPersonnes.ajouterPersonne("Sophie Dupont", p2);
+        
+        File img_p3 = new File("image_projet/homme_projet/3.jpg");
+        String[] affinite_p3 = {"Vegetarien","Arts","Cuisine","Musique","Sport","Voyage"};
+        Personne p3 = new Personne("Sophie Dupont",42,000,affinite_p3,img_p3);
+        donneesPersonnes.ajouterPersonne("Sophie Dupont", p3);
 
-	public Modele() {
-		
-	}
-
+        // Ajouter d'autres personnes ici...
+    }
+    
+    // Autres méthodes de la classe Modele...
 }
